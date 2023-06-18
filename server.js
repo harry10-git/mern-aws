377324de82eda721b3349bd2fdf93028f8d03912
Harry <harry@192.168.1.4>
2023-06-18 23:07:44 +0530

    pushing to github 3

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "client/build")))
 
 const _dirname = path.dirname("")
-const buildPath = path.join(_dirname  , "../client/build");
+const buildPath = path.join(_dirname  , "./client/build");
 app.use(express.static(buildPath))
 app.get("/*", function(req, res){
 
   res.sendFile(
-      path.join(__dirname, "../client/build/index.html"),
+      path.join(__dirname, "./client/build/index.html"),
       function (err) {
         if (err) {
           res.status(500).send(err);
