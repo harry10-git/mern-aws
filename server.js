d0dab99c61467a568ee4b219e72cc4b3f95c7234
Harry <harry@Harrys-MacBook-Air.local>
2023-06-19 15:37:28 +0530

    static front end done

--- server.js
+++ server.js
@@ -1,41 +1,42 @@
-// require("dotenv").config();
+require("dotenv").config();
 
 const express = require("express");
 var cors = require('cors')
+const path  = require('path')
+
 const mongoose = require("mongoose");
-
-// expres app
-const app = express();
-
-// for aws
-const path = require("path");
-
-app.use(express.static(path.join(__dirname, "client/build")))
-
-const _dirname = path.dirname("")
-const buildPath = path.join(_dirname  , "./client/build");
-app.use(express.static(buildPath))
-app.get("/*", function(req, res){
-
-  res.sendFile(
-      path.join(__dirname, "./client/build/index.html"),
-      function (err) {
-        if (err) {
-          res.status(500).send(err);
-        }
-      }
-    );
-
-})
 
 
 const workoutRoutes = require("./routes/workouts");
 
-
-app.use(cors())
-
+// expres app
+const app = express();
 //middleware
 app.use(express.json());
+
+
+// cors
+app.use(cors())
+
+
+// for aws 
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+// app.get("/*", function(req, res){
+
+//     res.sendFile(
+//         path.join(__dirname, "../client/build/index.html"),
+//         function (err) {
+//           if (err) {
+//             res.status(500).send(err);
+//           }
+//         }
+//       );
+
+// })
+
+
+
 
 app.use((req, res, next) => {
   // console.log(req.path, req.method);
@@ -45,6 +46,11 @@ app.use((req, res, next) => {
 // routes
 // fire routes only on api/api/workouts
 app.use("/api/workouts", workoutRoutes);
+
+// Redirect back to index.html if urls do not match
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"))
+})
 
 // connect to db
 mongoose
